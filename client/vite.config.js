8967ae0c93682387d465750a243672c07965f382
tshepogg <70196071+tshepogg@users.noreply.github.com>
2025-11-24 23:40:39 +0200

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,13 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/pdfs': 'https://ai-rag-hw.onrender.com',
-      '/ask': 'https://ai-rag-hw.onrender.com',
-      '/view': 'https://ai-rag-hw.onrender.com',
-    },
-  },
+  plugins: [react()]
+ 
 });
